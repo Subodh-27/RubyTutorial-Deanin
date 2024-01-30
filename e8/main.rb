1e196841c9ec664482b11e41ee46d8a27972e548
Subodh-27 <sbhawt@gmail.com>
2024-01-30 13:02:37 +0530

    Episode 8 Completed

---
+++ e8/main.rb
@@ -0,0 +1,19 @@
+# For Loop
+
+# Print Happy Holidays 5 times
+
+# 5.times do
+#     puts "Happy Holidays"
+# end
+
+# 5.times do | counter |
+#     puts "Happy Me times #{counter}"
+# end
+
+# (7..11).each do | counter |
+#     puts "The counter is #{counter}"
+# end
+
+(7...11).each do | counter |
+    puts "The counter is #{counter}"
+end

c98f1e0dde577fb7e64dd4204844641253a457c2
Subodh-27 <sbhawt@gmail.com>
2024-01-30 12:42:39 +0530

    Episode 6 Completed

---
+++ e6/main.rb
@@ -0,0 +1,31 @@
+# Conditionals
+
+# Legal Driving Age is 18
+# my_age = 20
+
+# if my_age >= 18
+#     puts "You Can Drive"
+# else
+#     puts "You Can't Drive"
+# end
+
+# Need to be Younger than 18 to go to School.
+# puts "Enter your Age for verification."
+# person_age = gets.chomp.to_i
+
+# if person_age < 18
+#     puts "You can go to School but You can't Drive"
+# else
+#     puts "You can't go to School but You can Drive"
+# end
+
+
+# Need to be 18 to vote
+my_age = 20
+puts "I am #{my_age} years old."
+
+if my_age == 18
+    puts "You can Vote"
+else
+    puts "You can't Vote"
+end

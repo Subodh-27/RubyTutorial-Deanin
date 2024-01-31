61b79479fd9c1d625559d6cf4c82d6332594baa4
Subodh-27 <sbhawt@gmail.com>
2024-01-31 16:16:42 +0530

    Episode 11 Completed

---
+++ e11/main.rb
@@ -0,0 +1,28 @@
+# Logical Operators
+
+# user_age = 17
+
+# if user_age >= 16 && user_age < 18
+#     puts "You can Drive with an Adult Only"
+# end
+
+
+# Check if Number is between 1 and 10 using AND Operator
+# puts "Enter a Number"
+# input_number = gets.chomp.to_i
+
+# if input_number > 0 && input_number < 11
+#     puts "The number is between 1 and 10"
+# else
+#     puts "The number is not between 1 and 10."
+# end
+
+# OR Operator
+# puts "Enter a Number"
+# input_number = gets.chomp.to_i
+
+# if input_number < 1 || input_number > 10
+#     puts "The number is not between 1 and 10"
+# else
+#     puts "The number is between 1 and 10."
+# end

5387541e544fbfc520fd22fc87cfc63bea17e2cf
Subodh-27 <sbhawt@gmail.com>
2024-01-31 18:45:54 +0530

    Episode 12 Completed

---
+++ e12/main.rb
@@ -0,0 +1,13 @@
+# Functions
+
+# def square_number(number_for_squaring)
+#     puts number_for_squaring * number_for_squaring
+# end
+# square_number(10)
+
+def multiply_func(first_number, second_number)
+    # return first_number * second_number
+    first_number * second_number
+end
+result = multiply_func(3, 5)
+puts "The multiplication result is = #{result}"
